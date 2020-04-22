d526b04100f39e498e306e7a496b82b2bfc58c6d
coderdream2018 <coderdream@gmail.com>
2020-04-22 16:48:27 +0800

    更新异常信息

--- WS01/WS01_framework.py
+++ WS01/WS01_framework.py
@@ -7,7 +7,7 @@ def getHTMLText(url):
         r.raise_for_status()  # 如果状态不是200，引发HTTPError异常
         r.encoding = r.apparent_encoding
         return r.text
-    exception:
+    except:
         return "产生异常"
 
 
